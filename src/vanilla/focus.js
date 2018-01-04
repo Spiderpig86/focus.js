8b48e6d8415d8f58ab428f4347c5b7c6c7a125b2
Stan <slim679975@gmail.com>
2018-01-04 13:38:49 -0500

    feat: Possibly completed the library

--- src/vanilla/focus.js
+++ src/vanilla/focus.js
@@ -5,12 +5,53 @@
 (function(window) {
 
     /**
+     * CONSTANTS
+     */
+    const ZOOMFACTOR = "250%"; // Default zoom factor of the image when user hovers with mouse
+
+    /**
      * Initializes the focus object with properties and methods
      */
     function init() {
         var Focus = {};
         Focus.init = function(elID) {
-            // TODO
+            // Iterate over all elements in the container by class
+            var container = document.getElementById(elID); // The container
+            var focusImgs = container.getElementsByClassName('.focus-img'); // Get all the elements in the container that support zooming
+
+            // Add handlers for each image element
+            focusImgs.forEach(function(img) {
+
+                // Add handler for mouse enter
+                img.addEventListener('mouseenter', function(e) {
+                    this.style.backgroundSize = ZOOMFACTOR; // Not even a lexical 'this' :(
+                }, false);
+
+                // Add handler for when the user moves the mouse around
+                img.addEventListener('mousemove', function(e) {
+
+                    // Get the bounding rectangle of the image (so only mouse movements inside that rectangle will register)
+                    var imgDimensions = this.getBoundingClientRect();
+
+                    // Calculate position of cursor relative to the image in pixels
+                    var x = e.clientX - imgDimensions.left;
+                    var y = e.clientY - imgDimensions.top;
+
+                    // Get the position of the cursor as a percentage of the total image size
+                    var percentX = Math.round(100 / (imgDimensions.width / x));
+                    var percentY = Math.round(100 / (imgDimensions.height / y));
+
+                    // Update the image background position
+                    this.style.backgroundPosition = percentX + '% ' + percentY + '%';
+
+                }, false);
+
+                // Add handler for when the user leaves
+                img.addEventListener('mouseleave', function(e) {
+                    this.style.backgroundPosition = "center";
+                    this.style.backgroundSize = "cover";
+                }, false);
+            });
         }
         return Focus;
     }
