611896ee0bb3059933d802787fe6fd37bf7ff5c4
Stan <slim679975@gmail.com>
2018-01-08 17:38:23 -0500

    feat(es6/focus.js): Added event binding, component properties, and rendering

--- src/es6/focus.js
+++ src/es6/focus.js
@@ -5,9 +5,12 @@
  class FocusImg {
 
     constructor(imageParams) {
+
+        this.DEFAULT_IMAGE_SRC = 'http://via.placeholder.com/500?text=focus.js';
+
         // Initialize default properties for image
         this.params = {
-            imageSrc: 'http://via.placeholder.com/500?text=focus.js',
+            imageSrc: this.DEFAULT_IMAGE_SRC,
             parentElement: null,
             zoomFactor: '250%',
             smoother: true
@@ -15,15 +18,62 @@
 
         this.focusImg = document.createElement('div');
 
+        // Load the image and try to get the height
+        let image = new Image();
+        image.onload = () => {
+            this.imgWidth = image.Width;
+            this.imgHeight = image.Height;
+        };
+        image.onerror = () => {
+            this.imageSrc = this.DEFAULT_IMAGE_SRC;
+            this.imgWidth = 500;
+            this.imgWidth = 500;
+        };
+
         // Apply given params to the object
         if (params)
             Object.assign(this.params, imageParams);
+
+        this.render();
+        this.bindEvents();
     }
 
+    /**
+     * Binds events to the current image component.
+     */
     bindEvents() {
 
+        // Zoom in on hover
+        this.focusImg.addEventListener('mouseover', (e) => {
+            this.focusImg.style.backgroundSize = this.params.zoomFactor;
+        }, false);
+
+        // Pan the image proportional to the cursor location
+        this.focusImg.addEventListener('mousemove', (e) => {
+            let dimensions = this.focusImg.getBoundingClientRect(); // Get client rectangle of the element on thepage
+
+            // Calculate location of cursor inside the element
+            let relX = e.clientX - dimensions.left;
+            let relY = e.clientY - dimensions.top;
+
+            // Calculate the cursor position as a percentage of the image
+            let percentX = Math.round(100 / (dimensions.width / relX));
+            let percentY = Math.round(100 / (dimensions.height / relY));
+
+            // Update the image background position
+            this.focusImg.style.backgroundPosition = percentX + '% ' + percentY + '%';
+        }, false);
+
+        // Revert image view back to normal after mouse exits
+        this.focusImg.addEventListener('mouseleave', (e) => {
+            this.focusImg.style.backgroundPosition = 'center';
+            this.focusImg.style.backgroundSize = 'cover';
+        }, false);
     }
 
+    /**
+     * Render the component onto the page
+     */
     render() {
         // Set the image element
         this.focusImg.innerHTML = `
@@ -41,3 +91,5 @@
         this.params.parentElement.appendChild(this.focusImg);
     }
  }
+
+ export default FocusImg;
