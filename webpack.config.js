f5fadd199e7ca77b51374d93d4762289c6d6bfae
Stan <slim679975@gmail.com>
2018-01-09 15:46:51 -0500

    chore(webpack.config.js): Updated webpack config with better compression

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack'); // Allowed by babel-loader
+const CompressionPlugin = require('compression-webpack-plugin')
 
 // Config for compilation
 let config = {
@@ -15,7 +16,32 @@ let config = {
                 loader: 'babel-loader'
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.optimize.AggressiveMergingPlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            mangle: true,
+            compress: {
+                warnings: false,
+                pure_getters: true,
+                unsafe: true,
+                unsafe_comps: true,
+                screw_ie8: true
+            },
+            output: {
+                comments: false
+            }
+        }),
+        new CompressionPlugin({
+            asset: "[path].gz[query]",
+            algorithm: "gzip",
+            test: /\.js$|\.css$|\.html$/,
+            threshold: 10240,
+            minRatio: 0
+        })
+    ]
 }
 
 module.exports = config;
